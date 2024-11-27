aabad6988df0a0f3a5454b35aaddfa1bd62d5415
Gabriel <44560145+GabrielLinharesBozzon@users.noreply.github.com>
2024-11-27 12:03:48 -0300

    arquivo de treinamento

---
+++ treino/TREINO2/index.js
@@ -0,0 +1,6 @@
+/**
+ * JS -Aula3: Vinculando um documento JS
+ *  @author Gabriel Linhares Bozzon
+ */
+document.write(`<h2>`+`Dev:`+`Gabriel Linhares Bozzon`+`</h2>`)
+
---
+++ treino/TREINO3/index.js
@@ -0,0 +1,15 @@
+/**
+ * JS - Funcao Simples
+ * @author Gabriel Linhares Bozzon
+ */
+
+
+function hello(){
+    var nome ;
+    nome = prompt("Qual e o seu nome?\n");
+    alert("Hello \n"+nome);
+
+}
+
+hello()
+
